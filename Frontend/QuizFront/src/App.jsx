0f2343c3421df096c3ac6c3ff523874b593e8f9a
Vagalēns <vagalehns@proton.me>
2025-06-24 14:37:37 +0300

    Set up react frontend. Added router and tailwind

---
+++ Frontend/QuizFront/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+        </a>
+        <a href="https://react.dev" target="_blank">
+        </a>
+      </div>
+      <h1 className='bg-red-500'>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
